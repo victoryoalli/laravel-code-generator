b8209c723882ebbb303315c732ae23ad045ca55b
Victor Yoalli <victoryoalli@gmail.com>
2021-04-11 23:52:34 -0500

    dbal 3 compability

--- src/Structure/Column.php
+++ src/Structure/Column.php
@@ -2,6 +2,8 @@
 
 namespace VictorYoalli\LaravelCodeGenerator\Structure;
 
+use Doctrine\DBAL\Types\Type;
+
 class Column
 {
     protected $_column;
@@ -14,13 +16,20 @@ class Column
 
     public function __construct($column)
     {
-        //parent::__construct();
         $this->_column = $column;
         $this->name = $this->_column->getName();
-        $this->type = str_replace("\\","",$this->_column->getType());
+        $this->type = $this->getTypeString($this->_column->getType());
         $this->length = $this->_column->getLength();
         $this->nullable = !$this->_column->getNotNull();
         $this->autoincrement = $this->_column->getAutoincrement();
         $this->default = $this->_column->getDefault();
+    }
+    private function getTypeString(string|Type $type)
+    {
+        return is_object($type)?preg_replace(
+            "/Doctrine\\\\DBAL\\\\Types\\\\([a-zA-B]+)Type/i",
+            '$1',
+            get_class($type)
+        ):str_replace("\\", "", $type);
     }
 }
