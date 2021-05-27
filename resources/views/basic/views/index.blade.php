94e59c157b61a6148c3c95dbf84019c0b6adbf4b
Victor Yoalli <victoryoalli@gmail.com>
2021-05-26 20:22:51 -0500

    wip

--- resources/views/basic/views/index.blade.php
+++ resources/views/basic/views/index.blade.php
@@ -4,29 +4,30 @@
 {!!code()->tag('x-guest-layout')!!}
 @endif
 
-<div class="">
+<div class="container mx-auto">
     @@if (session('status'))
-    <div class="">
+    <div class="bg-blue-100 font-medium max-w-4xl mx-auto my-4 px-8 py-4 rounded shadow text-blue-700 text-center text-sm ">
         @{{ session('status') }}
     </div>
     @@endif
     <div class="">
         <div class="">
-            <h1> {{str($model->name)->human()->title()->plural()}} </h1>
+            <h1 class="text-3xl text-blue-500 font-bold"> {{str($model->name)->human()->title()->plural()}} </h1>
         </div>
     <div class="">
 
-    <div>
-        <a href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.create'){{code()->doubleCurlyClose()}}">New</a>
+    <div class="flex justify-end">
+        <a class="text-blue-500" href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.create'){{code()->doubleCurlyClose()}}">New</a>
     </div>
-    <table class="">
+
+    <table class="border-gray-200 rounded shadow overflow-hidden mx-auto">
         @@if(count(${{str($model->name)->snake()->plural()}}))
-        <thead>
-            <tr>
+        <thead class="bg-gray-100 uppercase text-sm text-gray-500">
+            <tr class="">
                 <th>&nbsp;</th>
                 @foreach($model->table->columns as $column)
                 @if(!str($column->name)->matches('/id$/')&&!str($column->name)->matches('/_at$/'))
-                <td>{{str($column->name)->human()->title()}}</td>
+                <th class="px-6 py-3">{{str($column->name)->human()->title()}}</th>
                 @endif
 
                 @endforeach
@@ -34,13 +35,13 @@
 
         </thead>
         @@endif
-        <tbody>
+        <tbody class="bg-white divide-y divide-gray-100">
             @@forelse(${{str($model->name)->snake()->plural()}} as ${{str($model->name)->snake()}})
-            <tr>
-                <td>
-                    <a href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.show',['{{str($model->name)->snake()}}'=>${{str($model->name)->snake()}}] ){{code()->doubleCurlyClose()}}">Show</a>
-                    <a href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.edit',['{{str($model->name)->snake()}}'=>${{str($model->name)->snake($model->name)}}] ){{code()->doubleCurlyClose()}}">Edit</a>
-                    <a href="javascript:void(0)" onclick="event.preventDefault();
+            <tr class="">
+                <td class="px-6 py-4 text-xs">
+                    <a class=" text-blue-400 hover:text-blue-500" href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.show',['{{str($model->name)->snake()}}'=>${{str($model->name)->snake()}}] ){{code()->doubleCurlyClose()}}">Show</a>
+                    <a class=" text-blue-400 hover:text-blue-500" href="{{code()->doubleCurlyOpen()}}route('{{str($model->name)->slug()->plural()}}.edit',['{{str($model->name)->snake()}}'=>${{str($model->name)->snake($model->name)}}] ){{code()->doubleCurlyClose()}}">Edit</a>
+                    <a class=" text-blue-400 hover:text-blue-500" href="javascript:void(0)" onclick="event.preventDefault();
                     document.getElementById('delete-{{str($model->name)->slug()}}-{{code()->doubleCurlyOpen()}}${{str($model->name)->snake()}}->id{{code()->doubleCurlyClose()}}').submit();">
                         {{ __('Delete') }}
                     </a>
@@ -51,7 +52,7 @@
                 </td>
                 @foreach($model->table->columns as $column)
                 @if(!str($column->name)->matches('/id$/')&&!str($column->name)->matches('/_at$/'))
-                <td>{{code()->doubleCurlyOpen()}}${{str($model->name)->snake()}}->{{$column->name}}{{code()->doubleCurlyClose()}}</td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-600">{{code()->doubleCurlyOpen()}}${{str($model->name)->snake()}}->{{$column->name}}{{code()->doubleCurlyClose()}}</td>
                 @endif
                 @endforeach
 
