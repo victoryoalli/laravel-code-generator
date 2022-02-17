d3b6499089f1ef35700e6b8297ec9163745bc5b5
Victor Yoalli <victoryoalli@gmail.com>
2022-02-17 16:33:54 -0600

    removed str helper now available in L9

--- src/helpers.php
+++ src/helpers.php
@@ -1,20 +1,6 @@
 <?php
 
-use Illuminate\Support\Str;
 use VictorYoalli\LaravelCodeGenerator\Helper;
-
-if (! function_exists('str')) {
-    /**
-     * Create a collection from the given value.
-     *
-     * @param  mixed  $value
-     * @return \Illuminate\Support\Collection
-     */
-    function str(string $expression)
-    {
-        return Str::of($expression);
-    }
-}
 
 if (! function_exists('code')) {
     function code()
